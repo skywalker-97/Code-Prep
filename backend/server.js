e786602428901835c7a73ea654ba1b6b69902fbf
skywalker-97 <rajgaurav7250@gmail.com>
2026-02-14 20:14:26 +0530

    Open CORS fully

--- backend/server.js
+++ backend/server.js
@@ -8,15 +8,8 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 const app = express();
 
-const corsOptions = {
-  origin: "https://code-prep-frontend-ix5g.onrender.com",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.use(cors());
+app.options("*", cors());
 
 app.use(express.json());
 
